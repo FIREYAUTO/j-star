068c3ad540067a6a0c6094165a34db01351b5f0e
FIREYAUTO <56837610+FIREYAUTO@users.noreply.github.com>
2022-05-27 08:55:48 -0700

    Update JsT.js

--- src/JsT.js
+++ src/JsT.js
@@ -28,7 +28,7 @@ const Tokens = [
 
 const JsT = {
 	Split(Code=""){
-		return Code.split(/([^\w]|^\d+)/).filter(x=>!!x);
+		return Code.split(/([^\w])/).filter(x=>!!x);
 	},
 	Compress(List){
 		let Result = [],
@@ -63,17 +63,87 @@ const JsT = {
 			Stack = new BaseStack(List);
 		while(!Stack.IsEnd()){
 			let Token = Stack.Token;
-			if(Token.match(/\w+/)===Token){
-				Token.TYPE = "Identifier";	
+			let Tk = {Value:Token,Type:"Syntax"};
+			if(String(Token.match(/\w+/))===Token){
+				Tk.Type = "Identifier";
+			}if(Token==="\""){
+				let B=this.GetBetween(Stack,"\"","\"",true);
+				Tk.Value=this.CompileString(B);
+				Tk.Type = "String";
+			}if(Token==="'"){
+				let B=this.GetBetween(Stack,"'","'",true);
+				Tk.Value=this.CompileString(B);
+				Tk.Type = "String";
 			}
-			Result.push(Token);
+			Result.push(Tk);
+			Stack.Next();
 		}
 		return Result;
 	},
+	EscapeStringLiteral(Literal){
+		if(Literal.match(/^u[0-9]+/)){
+			return String.fromCharCode(+Literal.match(/[0-9]+/));
+		}
+		switch (Literal) {
+			case "r": return"\r";
+			case "n": return"\n";
+			case "b": return"\b";
+			case "t": return"\t";
+			case "c": return"\c";
+			case "f": return"\f";
+			case "v": return"\v";
+			case "t": return"\t";
+			default: return Literal;
+		}	
+	},
+	CompileString(List){
+		let R="";
+		for(let L of List){
+			let Add=undefined;
+			if(L.ESCAPED===true){
+				let M=L.match(/^u[0-9]+/),
+					LEN=1;
+				if(M)LEN=String(M).length;
+				if(L.length>1)Add=L.substr(LEN,L.length),L=L.substr(0,LEN);
+				L=this.EscapeStringLiteral(L);
+			}
+			R+=L;
+			if(Add)R+=Add;
+		}
+		return R;
+	},
+	GetBetween(Stack,T1,T2,AllowEscapes=false){
+		let Between = [];
+		if(Stack.Token===T1)Stack.Next();
+		if(Stack.Token===T2)return Between;
+		while(!Stack.IsEnd()){
+			if(Stack.Token===T2)break;
+			let T = Stack.Token;
+			if(AllowEscapes&&T==="\\"){
+				T=Stack.Next();
+				T.ESCAPED=true;
+			}
+			Between.push(T);
+			Stack.Next();
+		}
+		return Between;
+	},
 	RemoveWhitespace(List){
-		return List.filter(x=>!x.match(/\s/));
+		return List.filter(x=>x.Value.match(/\s+/)!=x.Value);
+	},
+	ApplyLineNumbers(List){
+		let Line=1,Index=1;
+		let R=[];
+		for(let L of List){
+			if(L.Value==="\n"||L.Value==="\r")Line++,Index=0;
+			L.Line=Line,
+				L.Index=Index;
+			Index+=L.Value.length;
+			R.push(L);
+		}
+		return R;	
 	},
 	Parse(Code){
-		return this.RemoveWhitespace(this.GenerateTypes(this.Compress(this.Split(Code))));
-	}
+		return this.RemoveWhitespace(this.ApplyLineNumbers(this.GenerateTypes(this.Compress(this.Split(Code)))));
+	},
 };
