82a442232e601076f695ffea8f02612daa112156
FIREYAUTO <56837610+FIREYAUTO@users.noreply.github.com>
2022-05-27 08:57:57 -0700

    Update JsT.js

--- src/JsT.js
+++ src/JsT.js
@@ -1,20 +1,3 @@
-//{{ Stack }}\\
-
-class BaseStack {
-	constructor(Tokens){
-		this.Tokens=Tokens,
-			this.Position=0,
-			this.Token=Tokens[0],
-			this.Length=Tokens.length;
-	}
-	Next(Amount=1){
-		return this.Token=this.Tokens[this.Position+=Amount];	
-	}
-	IsEnd(){
-		return this.Position >= this.Length;
-	}
-}
-
 //{{ Tokens }}\\
 
 const Tokens = [
